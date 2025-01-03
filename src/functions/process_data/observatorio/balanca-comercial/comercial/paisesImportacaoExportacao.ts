c42c9c92b3166ec8a6e9c6207fa83ce831746335
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-01-03 11:37:23 -0300

    new: all graphs from 'balança comercial' main page added

---
+++ src/functions/process_data/observatorio/balanca-comercial/comercial/paisesImportacaoExportacao.ts
@@ -0,0 +1,31 @@
+export const processImportacaoExportacaoPorPais = (data: any[]): any[] => {
+  const processedData: any = {};
+
+  data.forEach((item) => {
+    const pais = item["País"];
+    const valor = parseFloat(
+      (item["Valor US$"] || "0").replace(/\./g, "").replace(",", ".")
+    );
+    const tipo = item["tipo"];
+
+    if (!processedData[pais]) {
+      processedData[pais] = { pais, importacao: 0, exportacao: 0 };
+    }
+
+    if (processedData[pais].pais) {
+      if (tipo === "Importação") {
+        processedData[pais].importacao += valor;
+      } else if (tipo === "Exportação") {
+        processedData[pais].exportacao += valor;
+      }
+    }
+  });
+
+  // Ordena os dados por 'importacao' de maior para menor
+  const sortedData = Object.values(processedData)
+        .filter((item: any) => item.pais !== "")  // Filtra os itens com país vazio
+        .sort((a: any, b: any) => (b.importacao + b.exportacao) - (a.importacao + a.exportacao));  // Ordenação decrescente pelo total (importacao + exportacao)
+
+
+  return sortedData;
+};
