c322af6a2259d8d616df181bd7ff1cd796ddf6f8
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-02-20 11:59:25 -0300

    try to resolve trash commit

--- src/utils/filters/@global/getUniqueValues.ts
+++ src/utils/filters/@global/getUniqueValues.ts
@@ -11,3 +11,28 @@ export const getUniqueValues = <T, K extends keyof T>(data: T[], key: K): T[K][]
 
     return uniqueValues
 }
+
+
+export const getUniqueValuesArr = <T, K extends keyof T>(data: T[], key: K): { [key: string]: T[] }[] => {
+    // Cria um objeto para armazenar os valores agrupados por chave
+    const uniqueValuesMap: { [key: string]: T[] } = {};
+
+    // Itera sobre os dados
+    data.forEach((value) => {
+        const keyValue = String(value[key]);  // Converte o valor da chave para string
+
+        // Se a chave ainda não foi adicionada, inicializa o array
+        if (!uniqueValuesMap[keyValue]) {
+            uniqueValuesMap[keyValue] = [];
+        }
+
+        // Adiciona o item ao array correspondente à chave
+        uniqueValuesMap[keyValue].push(value);
+    });
+
+    // Retorna um array de objetos no formato solicitado
+    return Object.keys(uniqueValuesMap).map((key) => {
+        return { [key]: uniqueValuesMap[key] };
+    });
+};
+
