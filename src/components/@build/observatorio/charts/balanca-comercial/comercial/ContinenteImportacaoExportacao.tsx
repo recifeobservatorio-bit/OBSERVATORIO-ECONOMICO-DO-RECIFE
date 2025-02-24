36da4659886003a154706e09a3cb18d5d3fcd663
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-02-24 11:57:02 -0300

    add: new data on balança comercial tooltip charts - total percentage appears in the bars of the balanca comercial charts

--- src/components/@build/observatorio/charts/balanca-comercial/comercial/ContinenteImportacaoExportacao.tsx
+++ src/components/@build/observatorio/charts/balanca-comercial/comercial/ContinenteImportacaoExportacao.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import StackedBarChart from "@/components/@global/charts/StackedVerticalBarChart";
 import ColorPalette from "@/utils/palettes/charts/ColorPalette";
 import { processImportacaoExportacaoPorContinente } from "@/functions/process_data/observatorio/balanca-comercial/comercial/charts/continentesImportacaoExportacao";
@@ -8,28 +7,27 @@ import ChartGrabber from "@/components/@global/features/ChartGrabber";
 const ImportacaoExportacaoContinente = ({
   data = [],
   colors = ColorPalette.default,
-  title="Importação vs Exportação por Continente"
+  title = "Importação vs Exportação por Continente",
 }: any) => {
   const chartData = processImportacaoExportacaoPorContinente(data);
-
   return (
     <div className="chart-wrapper">
       <ChartGrabber>
         <StackedBarChart
-        data={chartData}
-        title={title}
-        colors={colors.slice(1)}
-        xKey="continente"
-        bars={[
-          { dataKey: "importacao", name: "Importação" },
-          { dataKey: "exportacao", name: "Exportação" },
-        ]}
-        tooltipEntry=" dólares"
-        heightPerCategory={80} // Define a altura de cada barra
-        visibleHeight={400} // Define a altura visível para scroll
-      />
+          data={chartData}
+          title={title}
+          colors={colors.slice(1)}
+          xKey="continente"
+          bars={[
+            { dataKey: "importacao", name: "Importação" },
+            { dataKey: "exportacao", name: "Exportação" },
+          ]}
+          tooltipEntry=" dólares"
+          heightPerCategory={80} // Define a altura de cada barra
+          visibleHeight={400} // Define a altura visível para scroll
+          chartType="continentesImportacaoExportacao" // Identifica o tipo de gráfico
+        />
       </ChartGrabber>
-      
     </div>
   );
 };
--- src/components/@build/observatorio/charts/balanca-comercial/comercial/PaisImportacaoExportacao.tsx
+++ src/components/@build/observatorio/charts/balanca-comercial/comercial/PaisImportacaoExportacao.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import StackerBarChartVertical from "@/components/@global/charts/StackedVerticalBarChart";
 import ColorPalette from "@/utils/palettes/charts/ColorPalette";
 import { processImportacaoExportacaoPorPais } from "@/functions/process_data/observatorio/balanca-comercial/comercial/charts/paisesImportacaoExportacao";
@@ -10,25 +9,24 @@ const ImportacaoExportacaoPorPais = ({
   colors = ColorPalette.default,
 }: any) => {
   const chartData = processImportacaoExportacaoPorPais(data);
-
   return (
     <div className="chart-wrapper">
       <ChartGrabber>
         <StackerBarChartVertical
-        data={chartData}
-        title="Importação vs Exportação por País"
-        colors={colors.slice(1)}
-        xKey="pais"
-        bars={[
-          { dataKey: "importacao", name: "Importação" },
-          { dataKey: "exportacao", name: "Exportação" },
-        ]}
-        tooltipEntry=" dólares"
-        heightPerCategory={60}  // Define a altura de cada categoria (barra)
-        visibleHeight={400}  // Ajuste a altura do scroll
-      />
+          data={chartData}
+          title="Importação vs Exportação por País"
+          colors={colors.slice(1)}
+          xKey="pais"
+          bars={[
+            { dataKey: "importacao", name: "Importação" },
+            { dataKey: "exportacao", name: "Exportação" },
+          ]}
+          tooltipEntry=" dólares"
+          heightPerCategory={60} // Define a altura de cada categoria (barra)
+          visibleHeight={400} // Ajuste a altura do scroll
+          chartType="paisesImportacaoExportacao" // Identifica o tipo de gráfico
+        />
       </ChartGrabber>
-      
     </div>
   );
 };
