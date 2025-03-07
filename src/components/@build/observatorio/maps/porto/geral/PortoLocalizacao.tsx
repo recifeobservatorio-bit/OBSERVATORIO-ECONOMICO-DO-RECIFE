04a13dce8ce86c760aedc3ae52f8dc2c86a7cb86
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-03-07 11:35:57 -0300

    fix: map data

--- src/components/@build/observatorio/maps/porto/geral/PortoLocalizacao.tsx
+++ src/components/@build/observatorio/maps/porto/geral/PortoLocalizacao.tsx
@@ -8,6 +8,7 @@ type PortoData = {
   Longitude: string;
   Latitude: string;
   "Porto Atracação": string;
+  Mes: number;
   VLPesoCargaBruta: number;
 };
 
@@ -19,7 +20,7 @@ type GeoJSONFeature = {
   };
   properties: {
     name: string;
-    qtCarga: number;
+    vlPesoCargaBruta: number;
   };
 };
 
@@ -28,15 +29,34 @@ type GeoJSON = {
   features: GeoJSONFeature[];
 };
 
-// Função para converter JSON em GeoJSON
-function convertToGeoJSON(data: PortoData[]): GeoJSON {
+function convertToGeoJSON(data: PortoData[], selectedMonths: number[]): GeoJSON {
   if (!Array.isArray(data)) {
     console.error("A data deve ser um array.");
     return { type: "FeatureCollection", features: [] };
   }
+
+  const filteredData = selectedMonths.length > 0
+    ? data.filter((item) => selectedMonths.includes(item.Mes))
+    : data;
+
+  const aggregatedData: { [key: string]: PortoData } = {};
+
+  filteredData.forEach((item) => {
+    const key = item["Porto Atracação"];
+
+    if (!aggregatedData[key]) {
+      aggregatedData[key] = { ...item };
+    } else {
+
+      aggregatedData[key].VLPesoCargaBruta += item.VLPesoCargaBruta;
+    }
+  });
+
+  const aggregatedDataArray = Object.values(aggregatedData);
+
   return {
     type: "FeatureCollection",
-    features: data.map((item) => ({
+    features: aggregatedDataArray.map((item) => ({
       type: "Feature",
       geometry: {
         type: "Point",
@@ -47,42 +67,43 @@ function convertToGeoJSON(data: PortoData[]): GeoJSON {
       },
       properties: {
         name: item["Porto Atracação"],
-        qtCarga: parseInt(item.VLPesoCargaBruta as any),
+        vlPesoCargaBruta: item.VLPesoCargaBruta,
       },
     })),
   };
 }
 
 type PortoLocalizacaoProps = {
-  data: PortoData[];
+  data: [PortoData[], { label: string; options: number[]; selected: number[] }];
 };
 
 export default function PortoLocalizacao({ data }: PortoLocalizacaoProps) {
   const mapRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    if (!mapRef.current) return;
 
-    // Remove qualquer mapa existente antes de criar um novo
-    if ((mapRef.current as any)._leaflet_id) {
-      mapRef.current.innerHTML = "";
+    if (!mapRef.current || !data || !Array.isArray(data) || data.length < 2) {
+      console.error("Os dados estão ausentes ou na estrutura incorreta.");
+      return;
     }
 
-    // Inicializa o mapa
-    const map = L.map(mapRef.current).setView([-14.673, -51.260], 4); // Coordenadas iniciais e zoom
+    const portoData = data[0];
+    const selectedMonths = data[1].selected;
+
+    const map = L.map(mapRef.current).setView([-14.673, -51.260], 4);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 18,
       attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
 
-    const geoJsonData = convertToGeoJSON(data);
+    const geoJsonData = convertToGeoJSON(portoData, selectedMonths);
 
     L.geoJSON(geoJsonData, {
       pointToLayer: (feature, latlng) => {
         const fillColor = feature.properties.name === "Recife" ? "red" : ColorPalette.default[1];
         const { coordinates } = feature.geometry;
-        const { name, qtCarga } = feature.properties;
+        const { name, vlPesoCargaBruta } = feature.properties;
 
         const circleMarker = L.circleMarker(latlng, {
           radius: 8,
@@ -93,14 +114,12 @@ export default function PortoLocalizacao({ data }: PortoLocalizacaoProps) {
           fillOpacity: 0.8,
         });
 
-        // Adiciona o balão de informação ao círculo
         circleMarker.bindPopup(`
-          <div
-          ">
+          <div>
             <h3 style="margin: 0; color: #0077cc; font-size: 17px;">⚓ ${name}</h3>
             <hr style="border: 0; height: 1px; background: #ddd; margin: 8px 0;">
             <p style="margin: 0; font-size: 14px;"><strong>Carga:</strong></p>
-            <p style="margin: 0; font-size: 13px; font-weight: bold; color: #333;">${qtCarga} (ton)</p>
+            <p style="margin: 0; font-size: 13px; font-weight: bold; color: #333;">${vlPesoCargaBruta} (ton)</p>
           </div>
         `);
 
@@ -111,7 +130,7 @@ export default function PortoLocalizacao({ data }: PortoLocalizacaoProps) {
     return () => {
       map.remove();
     };
-  }, [data]);
+  }, [data[1]?.selected]);
 
   return <div ref={mapRef} style={{ height: "500px", width: "100%" }} />;
 }
