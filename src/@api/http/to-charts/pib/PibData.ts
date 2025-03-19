eae2e353cdf87b103858b3626f3693dec20d3f9e
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-03-19 16:42:49 -0300

    change: pib fetch to use bundle data

--- src/@api/http/to-charts/pib/PibData.ts
+++ src/@api/http/to-charts/pib/PibData.ts
@@ -1,10 +1,9 @@
 import { ProcessedDataPib } from "@/@types/observatorio/pib/ProcessedDataPib";
-import { parquetRead } from "hyparquet";
-import { compressors } from "hyparquet-compressors";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const API_USERNAME = process.env.NEXT_PUBLIC_API_USERNAME;
-const API_PASSWORD = process.env.NEXT_PUBLIC_API_PASSWORD;
+import { fetchData } from "@/@api/config/dataFetcher";
+
+const DB_NAME = "parquetDB";
+const STORE_NAME = "parquetFiles";
 
 export class PibData {
   private year: string;
@@ -14,71 +13,15 @@ export class PibData {
     this.year = year;
   }
 
-  private async fetchData<T>(endpoint: string): Promise<T> {
-    if (PibData.cache[endpoint]) {
-      console.log("Usando dados em cache para:", endpoint);
-      return PibData.cache[endpoint];
-    }
-
-    try {
-      const response = await fetch(`${BASE_URL}${endpoint}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Basic ${btoa(`${API_USERNAME}:${API_PASSWORD}`)}`, // Autenticação básica
-        },
-        credentials: "include",
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erro ao buscar dados da API: ${endpoint}`);
-      }
-
-      const contentType = response.headers.get("content-type");
-      let data: any;
-
-      if (contentType?.includes("application/octet-stream")) {
-        const startTime = performance.now();
-        const arrayBuffer = await response.arrayBuffer();
-
-        const records = await new Promise((resolve, reject) => {
-          parquetRead({
-            file: arrayBuffer,
-            rowFormat: "object",
-            compressors,
-            onComplete: (data) => resolve(data),
-          });
-        });
-
-        const endTime = performance.now();
-        console.log(`Tempo de execução: ${(endTime - startTime).toFixed(2)} ms`);
-        data = records;
-      } else {
-        // Caso o conteúdo seja JSON
-        const text = await response.text();
-        data = JSON.parse(text);
-      }
-
-      // Armazena os dados em cache
-      PibData.cache[endpoint] = data;
-
-      return data;
-    } catch (error) {
-      console.error(`Erro em fetchData (${endpoint}):`, error);
-      throw error;
-    }
-  }
 
   async fetchProcessedData(): Promise<ProcessedDataPib[]> {
     const endpoint = `/pib/geral/anos/${this.year}`;
-    // const endpoint = `/balanco-comercial/geral/${this.year}`;
-    return this.fetchData<ProcessedDataPib[]>(endpoint);
+    return fetchData<ProcessedDataPib[]>(endpoint, PibData.cache);
   }
 
   async fetchProcessedDataByYear(year: string): Promise<ProcessedDataPib[]> {
     const endpoint = `/pib/geral/anos/${year}`;
-    // const endpoint = `/balanco-comercial/geral/${this.year}`;
-    return this.fetchData<ProcessedDataPib[]>(endpoint);
+    return fetchData<ProcessedDataPib[]>(endpoint, PibData.cache);
   }
 
   // Limpa o cache de dados
