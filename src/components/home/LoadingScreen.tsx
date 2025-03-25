63e31e52c396435c81e2904f700c19476d6dea1b
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-03-25 16:28:42 -0300

    fix: minimize loading only when first loading

--- src/components/home/LoadingScreen.tsx
+++ src/components/home/LoadingScreen.tsx
@@ -79,7 +79,8 @@ export const LoadingScreen = () => {
       <div className="flex flex-col items-center z-20">
         <button
           onClick={() => setIsMinimized(true)}
-          className="absolute top-4 right-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition-colors duration-200"
+          className={`absolute top-4 right-4 p-2 bg-gray-200 rounded-full hover:bg-gray-300 transition-colors duration-200 
+            ${isFirstLoad ? "" : "hidden"} `}
         >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
