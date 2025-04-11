c0134c5e17612a255e067ea93c789fbccccc4ec1
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-04-11 07:58:58 -0300

    change: check mobile sizes

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -4,7 +4,7 @@ export const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth < 768 || window.innerHeight < 768); // breakpoint pra detectar se é mobile
+    const checkMobile = () => setIsMobile(window.innerWidth < 768 || window.innerHeight < 768 && window.innerWidth < 500); // breakpoint pra detectar se é mobile
     checkMobile();
     window.addEventListener("resize", checkMobile);
     return () => window.removeEventListener("resize", checkMobile);
