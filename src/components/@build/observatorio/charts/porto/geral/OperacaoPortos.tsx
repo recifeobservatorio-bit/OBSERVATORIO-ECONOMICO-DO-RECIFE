426fa3b73b54b8217fa2034f28e033cdea3c367e
ThiSilva49 <thiago.fernando1129@gmail.com>
2025-04-12 10:21:59 -0300

    fix: porto file name

--- src/components/@build/observatorio/charts/porto/geral/OperacaoPortos.tsx
+++ src/components/@build/observatorio/charts/porto/geral/OperacaoPortos.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ScrollableBarChart from "@/components/@global/charts/VerticalScrollableBarChart";
 import ColorPalette from "@/utils/palettes/charts/ColorPalette";
 import ChartGrabber from "@/components/@global/features/ChartGrabber";
-import { processCargasPorPorto } from "@/functions/process_data/observatorio/porto/geral/charts/opecaoPorPorto";
+import { processCargasPorPorto } from "@/functions/process_data/observatorio/porto/geral/charts/operacaoPorPorto";
  
 const OperacaoPortos = ({
   data,
